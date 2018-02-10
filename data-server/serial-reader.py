a3d5d9d899842309c32fce820a4b78e03570a1f3
subzero <dima@svalov.ru>
2018-02-10 19:20:52 +0300

    v.1

--- data-server/serial-reader.py
+++ data-server/serial-reader.py
@@ -2,10 +2,15 @@ import argparse
 import datetime
 import json
 import logging
-import os.path as op
 import uuid
-
 import serial
+
+from os.path import normpath, join
+
+logging.basicConfig(format='%(asctime)-15s %(message)s')
+logger = logging.getLogger('serial-reader')
+logger.setLevel(level=logging.INFO)
+logger.info('serial-reader started')
 
 parser = argparse.ArgumentParser(description='Device Data Serial Reader.')
 parser.add_argument('--serial', nargs=1, type=str, required=True, help='serial port address')
@@ -14,16 +19,12 @@ args = parser.parse_args()
 sport = args.serial[0]
 mdir = args.dir[0]
 
-logging.basicConfig(format='%(asctime)-15s %(message)s')
-logger = logging.getLogger('serial-reader')
-logger.setLevel(level=logging.INFO)
-logger.info('serial-reader started')
-
-logger.debug('Starting to read ' + sport)
+logger.info('Reading from ' + sport)
 ser = serial.Serial(sport, 9600)
 while True:
     data = ser.readline()
     logger.debug(data)
+    logger.info('{0} bytes read'.format(len(data)))
     msg = int(data)
     logger.debug(msg)
     deviceId = msg >> 28 & 0xF
@@ -32,7 +33,12 @@ while True:
     fname = str(uuid.uuid1())
     jm = ''
     if deviceId == 0:
-        jm = json.dumps({'deviceId': deviceId, 'distance': (msg >> 16) & 0xFFF, 'timestamp': curtime}, indent=4)
+        jm = json.dumps({
+            'deviceId': deviceId,
+            'distance': (msg >> 16) & 0xFFF,
+            'timestamp': curtime},
+            sort_keys=True,
+            indent=4)
         logger.debug(jm)
     elif deviceId == 1:
         jm = json.dumps({
@@ -41,6 +47,7 @@ while True:
             'temperature': (msg >> 15) & 0x3F,
             'rain': (msg >> 13) & 0x3,
             'timestamp': curtime},
+            sort_keys=True,
             indent=4)
         logger.debug(jm)
     elif deviceId == 2:
@@ -49,6 +56,7 @@ while True:
             'humidity': (msg >> 16) & 0xFFF,
             'temperature': msg & 0xFFFF,
             'timestamp': curtime},
+            sort_keys=True,
             indent=4)
         logger.debug(jm)
     elif deviceId == 3:
@@ -57,11 +65,13 @@ while True:
             'pressure': (msg >> 12) & 0x3FF,
             'temperature': (msg >> 22) & 0x3F,
             'timestamp': curtime},
+            sort_keys=True,
             indent=4)
         logger.debug(jm)
     else:
         logger.error('Unsupported deviceId ' + str(deviceId) + ', message: ' + str(data))
         continue
-    f = open(op.normpath(op.join(mdir, fname)), "w")
+    f = open(normpath(join(mdir, fname)), "w")
     f.write(jm)
     f.close()
+    logger.info('Saved sensor data to file {0}'.format(fname))
